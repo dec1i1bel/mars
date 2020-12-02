48f22f780e3d8110da5ec55b4267221c1d2bcfc8
Vasiliy Balabanov <v.balabanov85@gmail.com>
2020-12-02 23:45:10 +0300

    init

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>onMars</title>
+    <script src="libs/jquery-3.5.1.min.js"></script>
+    <script src="libs/bootstrap-4.5.3-dist/js/bootstrap.bundle.min.js"></script>
+    <script src="libs/fancybox3/dist/jquery.fancybox.min.js"></script>
+    <script src="libs/moment.min.js"></script>
+    <script src="scripts.js"></script>
+    <link rel="stylesheet" href="libs/bootstrap-4.5.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="libs/fancybox3/dist/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="rover-name">Марсоход: <span class="value"></span></h1>
+        <h3 class="rover-sol">Суток пребывания на планете: <span class="value"></span></h3>
+        <h3 class="rover-photos-number">Итого снято фотографий: <span class="value"></span></h3>
+        <p></p>
+        <h2 class="rover-photos-title">Фото за <span class="value date"></span> (всего: <span class="value quantity_total"></span>, больших: <span class="value quantity_large"></span>)
+        </h2>
+        <p>
+        <h4>Фильтры</h4>
+        </p>
+        <div id="filters" class="js-filters">
+            <div class="js-only-large">
+                <input type="checkbox" name="only-large">
+                <label class="form-check-label" for="only-large-photos">Только большие фото</label>
+            </div>
+        </div>
+        <div class="btns_next-prev-date row justify-content-center" current_date="">
+            <button class="btn-prev-day btn btn-outline-success">Назад</button>
+            <div class="mx-2 py-3">на 1 день</div>
+            <button class="btn-next-day btn btn-outline-success">Вперёд</button>
+        </div>
+        <div id="photos__page" class="row row-cols-2 row-cols-md-3 row-cols-lg-4 justify-content-center"></div>
+        <div class="btns_next-prev-date row justify-content-center">
+            <button class="btn-prev-day btn btn-outline-success">Назад</button>
+            <div class="mx-2 py-3">на 1 день</div>
+            <button class="btn-next-day btn btn-outline-success">Вперёд</button>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ scripts.js
@@ -0,0 +1,139 @@
+$(document).ready(function () {
+    let rovers = ['curiosity', 'oportunity', 'spirit'],
+        rover = rovers[0],
+        roverName = rover[0].toUpperCase() + rover.substring(1)
+    ;
+    let next_prev = '.btns_next-prev-date',
+        cl_ph_num = '.rover-photos-number > .value',
+        curdate = '.rover-photos-title > .date',
+        attr_curdate = 'current_date'
+    ;
+    var showOnlyLargePhotos = false,
+        isLargeImg = true,
+        ph_num_all = 0,
+        ph_num_large = 0,
+        api_key = 'tHmV7JS4rx9Jm4uXHtMs9rEbCvQOCLSfnjPus886'
+    ;
+
+    $.ajax({
+        url: 'https://api.nasa.gov/mars-photos/api/v1/manifests/' + rover + '?api_key='+api_key,
+        success: function (data) {
+            console.log(data.photo_manifest);
+            let max_date = data.photo_manifest.max_date;
+
+            // шапка - общая статистика
+            $('.rover-sol > .value').text(data.photo_manifest.max_sol);
+            $(cl_ph_num).text(data.photo_manifest.total_photos);
+            $(curdate).text(max_date);
+            $(next_prev).attr(attr_curdate, max_date)
+
+            //последний день, за который есть фотографии
+            $.ajax({
+                url: 'https://api.nasa.gov/mars-photos/api/v1/rovers/' + rover + '/photos?earth_date=' + max_date + '&api_key='+api_key,
+                success: function (data_latest) {
+                    renderAjaxImg(data_latest, max_date);
+                }
+            });
+        }
+    });
+
+    // клик по кнопке предыдущего / следующего дня
+    $('.btn-prev-day').each(function () {
+        $(this).click(function () {
+            renderNearestDayPhotos($(this));
+        })
+    });
+    $('.btn-next-day').each(function () {
+        $(this).click(function () {
+            renderNearestDayPhotos($(this));
+        })
+    });
+
+    $('.js-only-large').find('input[name="only-large"]').click(function() {
+        let activeDateTmpst = moment($(next_prev).attr(attr_curdate));
+        let activeDate = activeDateTmpst.format('YYYY-MM-DD');
+        $.ajax({
+            url: 'https://api.nasa.gov/mars-photos/api/v1/rovers/' + rover + '/photos?earth_date=' + activeDate + '&api_key='+api_key,
+            success: function (data_near) {
+                renderAjaxImg(data_near, activeDate);
+            }
+        })
+    })
+
+    $('.rover-name > .value').text(roverName);
+
+    $('.js-only-large').click(function () {
+        if ($(this).find('input[name="only-large"]').is(':checked')) {
+            showOnlyLargePhotos = true;
+        } else {
+            showOnlyLargePhotos = false;
+        }
+    })
+
+    function renderAjaxImg(data, date = '') {
+        $('#photos__page').html('');
+        if (data.photos.length) {
+
+            let ph = data.photos;
+            ph_num_all = 0;
+            ph_num_large = 0;
+
+            $(curdate).text(date);
+            $(next_prev).attr(attr_curdate, date);
+            $(next_prev).last().show();
+
+            console.log('showOnlyLargePhotos');
+            console.log(showOnlyLargePhotos);
+
+            for (let i = 0; i < ph.length; i++) {
+
+                img = new Image();
+                img.src = ph[i].img_src;
+                img.onload = function () {
+                    if (this.width < 1000) {
+                        isLargeImg = false;
+                    } else {
+                        console.log('isLargeImg = true');
+                        isLargeImg = true;
+                        if(showOnlyLargePhotos) {
+                            appendPhoto(i, ph[i].img_src);
+                        }
+                        ph_num_large++;
+                    }
+                        $('.quantity_large').text(ph_num_large);
+                }
+                if (!showOnlyLargePhotos) {
+                    appendPhoto(i, ph[i].img_src);
+                }
+
+                ph_num_all++;
+            }
+            $('.quantity_total').text(ph_num_all);
+        } else {
+            $(curdate).text(date);
+            $('.quantity_total').text('нет');
+            $('.quantity_large').text('нет');
+            $(next_prev).attr(attr_curdate, date);
+            $('#photos__page').html('');
+            $(next_prev).last().hide();
+        }
+    }
+
+    function appendPhoto(i,img) {
+        $('#photos__page').append('<div class="card-deck"><div class="card"><a data-fancybox="gallery" href="' + img + '"><img src="' + img + '" class="photo d-block w-100" alt="..."></a><!--<div class="card-body"><select name="photo_' + i + '" class="form-control form-control-sm"><option value="novalue">---</option><option value="landscape">ландшафт</option><option value="rover">марсоход</option></select></div>--></div></div>');
+    }
+    
+
+    function renderNearestDayPhotos($btn) {
+        let currentDate = moment($(next_prev).attr(attr_curdate));
+        let numday;
+        $btn.hasClass('btn-prev-day') ? numday = -1 : numday = 1
+        let nearDate = currentDate.add(numday, 'day').format('YYYY-MM-DD');
+        $.ajax({
+            url: 'https://api.nasa.gov/mars-photos/api/v1/rovers/' + rover + '/photos?earth_date=' + nearDate + '&api_key='+api_key,
+            success: function (data_near) {
+                renderAjaxImg(data_near, nearDate);
+            }
+        })
+    }
+});
