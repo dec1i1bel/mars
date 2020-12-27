93bed03c7454e076eb1c2dc77af2fa076e878f20
Vasiliy Balabanov <v.balabanov85@gmail.com>
2020-12-27 16:15:34 +0300

    файлы перемещены в корень, удалены файлы библиотек, библиотеки подключены по cdn

--- index.php
+++ index.php
@@ -5,15 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>mars</title>
-    <script src="libs/jquery-3.5.1.min.js"></script>
-    <script src="libs/bootstrap-4.5.3-dist/js/bootstrap.bundle.min.js"></script>
-    <script src="libs/fancybox3/dist/jquery.fancybox.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
     <script src="libs/moment.min.js"></script>
     <script src="libs/jquery-ui-1.12.1/jquery-ui.js"></script>
     <script src="https://kit.fontawesome.com/689a977d1f.js" crossorigin="anonymous"></script>
     <script src="scripts.js?v=4545"></script>
-    <link rel="stylesheet" href="libs/bootstrap-4.5.3-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="libs/fancybox3/dist/jquery.fancybox.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+	<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+
     <link rel="stylesheet" href="libs/jquery-ui-1.12.1/jquery-ui.css">
     <link rel="stylesheet" href="styles.css?v=787">
 </head>
