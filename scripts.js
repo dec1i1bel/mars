c91fbced74d47523af2a73be73ea98d173b8cc90
vasiliy <v.balabanov@ddemo.ru>
2021-01-02 13:34:03 +0300

    не работает кнопка установка на мобильном. но можно установить из мею браузера

--- scripts.js
+++ scripts.js
@@ -210,3 +210,37 @@ $(document).ready(function () {
 		dateFormat: "yy-mm-dd"
 	});
 });
+
+// Register service worker to control making site work offline
+
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then(function() { console.log('Service Worker Registered'); });
+  }
+  
+  // Code to handle install prompt on desktop
+  
+  var deferredPrompt;
+  var addBtn = document.querySelector('.add-button');
+  addBtn.style.display = 'none';
+  
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = 'block';
+  
+    addBtn.addEventListener('click', (e) => {
+      // hide our user interface that shows our A2HS button
+      addBtn.style.display = 'none';
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+          deferredPrompt = null;
+        });
+    });
+  });
---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('mars-photos').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/scripts.js',
+                '/styles.css',
+                '/libs/'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log(e.request.url);
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  });
